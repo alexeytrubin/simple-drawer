3a8c45662e09dd4a881fb91d5c33b6bc70048bef
Alexey Trubin <alxjnk@gmail.com>
2025-03-05 08:56:21 +0000

    feat. added deplot gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/simple-drawer/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
